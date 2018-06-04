d78ca5055145f6ed7e94dd22370c81ede7566aae
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-04 08:25:47 -0400

    DP-3:

--- Visitor/java/Visitor/src/main/java/io/github/mchirico/HouseRoomDisplayVisitor.java
+++ Visitor/java/Visitor/src/main/java/io/github/mchirico/HouseRoomDisplayVisitor.java
@@ -2,24 +2,31 @@ package io.github.mchirico;
 
 public class HouseRoomDisplayVisitor implements HouseRoomVisitor {
 
+    int bedrooms;
+    int doors;
+
     @Override
     public void visit(House house) {
-        System.out.println("Entering the house.");
+        System.out.println("Entering the house. " +
+                "Door: "+ ++doors);
     }
 
     @Override
     public void visit(Garage garage) {
-        System.out.println("In garage.");
+        System.out.println("In garage. " +
+                "Door: "+ ++doors);
     }
 
     @Override
     public void visit(LivingRoom livingRoom) {
-        System.out.println("In livingroom.");
+        System.out.println("In livingroom. " +
+                "Door: "+ ++doors);
     }
 
     @Override
     public void visit(BedRoom bedRoom) {
-        System.out.println("In bedroom.");
+        System.out.println("In bedroom " + ++bedrooms +
+        " Door: "+ ++doors);
     }
 
 }
