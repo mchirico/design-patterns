503730325d14143451c882cdfa22a2ea9118b447
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-09 10:57:44 -0400

    DP-21: Firebase
    DP-21: dir
    
    DP-21: Added encrypted file
    
    dp-21
    
    moved warnings

---
+++ TestTemplate/java/T0/src/main/java/Abby.java
@@ -0,0 +1,15 @@
+public class Abby {
+    int sum = 0;
+
+    Abby(int i) {
+        sum += i;
+    }
+
+    void add(int i) {
+        sum += i;
+    }
+
+    int getSum() {
+        return sum;
+    }
+}
--- TestTemplate/java/T0/src/main/java/Main.java
+++ TestTemplate/java/T0/src/main/java/Main.java
@@ -3,21 +3,31 @@
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-        GetBillInput getBillInput = new GetBillInput();
+        System.out.println(System.getProperty("user.home"));
+        FireBaseExample fb = new FireBaseExample();
+        fb.init();
 
+        Abby abby = new Abby(30);
+        System.out.println(abby.getSum());
+        abby.add(10);
+        System.out.println(abby.getSum());
 
-        do {
-            getBillInput.repeatAskingForBillUntilValid();
-            getBillInput.repeatAskingForTipUntilValid();
-
-            getBillInput.repeatAskingForNumberOfPeopleUntilValid();
-            System.out.print(String.format("Each person pays: %.2f", getBillInput.splitBill()));
-
-        } while (getBillInput.repeatAskingYesNoUntilValid());
-
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(String.format("HI: %d", i));
+        }
     }
 }
+
+//        GetBillInput getBillInput = new GetBillInput();
+//
+//
+//        do {
+//            getBillInput.repeatAskingForBillUntilValid();
+//            getBillInput.repeatAskingForTipUntilValid();
+//
+//            getBillInput.repeatAskingForNumberOfPeopleUntilValid();
+//            System.out.print(String.format("Each person pays: %.2f", getBillInput.splitBill()));
+//
+//        } while (getBillInput.repeatAskingYesNoUntilValid());
