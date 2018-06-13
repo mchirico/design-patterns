2514027dc1025364e34f520ebb099e7e98bfc435
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-13 12:54:48 -0400

    DP-22: C Sharp

---
+++ TestTemplate/cSharp/ConsoleWithTest/ConsoleWithTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleToAttribute("xUnitTestStuff")]
+
+namespace ConsoleWithTest
+{
+
+    public class TestClass
+    {
+        private int testValue = 42;
+
+        private int TestMethod(int numberToAdd)
+        {
+            return this.testValue + numberToAdd;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var lines = ReadFrom(@"/Users/mchirico/SampleQuotes.txt");
+
+            using (System.IO.StreamWriter file =
+                   new System.IO.StreamWriter(@"/Users/mchirico/garbo.txt"))
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+                file.WriteLine(line);
+            }
+            
+        }
+
+         private int Do(int a){
+            var lines = ReadFrom(@"/Users/mchirico/SampleQuotes.txt");
+
+            using (System.IO.StreamWriter file =
+                   new System.IO.StreamWriter(@"/Users/mchirico/garbo.txt"))
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                    file.WriteLine(line);
+                }
+            return a;
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
