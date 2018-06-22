780cc4c26985ad6abffdccb9c5c65287b4203810
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-06-22 05:27:50 -0400

    DP-31: Bridge simple... no main
    DP-31: Start of working bridge
    
    DP-31: move around
    
    DP-31: clean up

--- Go/src/bridge/bridge_test.go
+++ Go/src/bridge/bridge_test.go
@@ -1,20 +1,34 @@
 package bridge
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
-//func TestMain(m *testing.M) {
-//	flag.Parse()
-//	os.Exit(m.Run())
-//}
 
-func TestMyrun(t *testing.T) {
+func TestGreenCircle(t *testing.T) {
+	g := greenStruct{}
+	b := blueStruct{}
 
-	var ans = Myrun(3)
-	if ans != 6 {
-		t.Fail()
-		t.Log("We expected to get something else")
+	c := circleStruct{}
+	fmt.Printf("Circle: %v\n", c.Circle(0, 2, 3, g))
+	fmt.Printf("Circle: %v\n", c.Circle(0, 2, 3, b))
+
+	expected := "Color: Blue, x: 0 y: 2 r: 3"
+
+	if strings.Compare(expected, c.Circle(0, 2, 3, b)) != 0 {
+		t.Errorf("Strings don't match"+
+			"\n%v\n%v", expected, c.Circle(0, 2, 3, b))
 	}
+
+}
+
+func TestDoSomething(t *testing.T) {
+	DoSomething("stuff")
+}
+
+func TestReflect(t *testing.T) {
+	DoReflect()
 
 }
